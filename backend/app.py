e8a66d81ddf9710bfc098e2a8dd51e4440bbeeb3
B34P4S4 <netkataster@gmail.com>
2026-05-24 17:44:41 +0200

    frontend con dashboard inicial

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from backend.core.agents.agent import analizar_proyecto
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/analiza", methods=["POST"])
 def analiza():
